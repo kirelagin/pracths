eb9f6e97ca8cb75962c5cc20532c3399e71c9e50
Kirill Elagin <kirelagin@gmail.com>
2019-11-22 11:31:19 +0300

    wc: Use Monoid

--- apps/wc/Main.hs
+++ apps/wc/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char (isSpace)
+import Data.Monoid (Sum (Sum))
 
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Lazy.Char8 as C8
@@ -14,13 +15,36 @@ main = do
   run fp
 
 
-data CountResult = CountResult
-  { crLines :: !Int
-  , crWords :: !Int
-  , crPrevW :: !Bool
-  , crBytes :: !Int
+data WordsResult = WordsResult
+  { wrStartW :: !Bool
+  , wrWords :: {-# UNPACK #-} !Int
+  , wrEndW :: !Bool
   }
-  deriving Show
+
+instance Semigroup WordsResult where
+  WordsResult s1 w1 e1 <> WordsResult s2 w2 e2 =
+    WordsResult s1 (w1 + w2 - if e1 && s2 then 1 else 0) e2
+
+instance Monoid WordsResult where
+  mempty = WordsResult False 0 False
+
+
+data CountResult = CountResult
+  { crLines :: {-# UNPACK #-} !(Sum Int)
+  , crWords :: {-# UNPACK #-} !WordsResult
+  , crBytes :: {-# UNPACK #-} !(Sum Int)
+  }
+
+instance Semigroup CountResult where
+  CountResult l1 w1 b1 <> CountResult l2 w2 b2 =
+    CountResult (l1 <> l2) (w1 <> w2) (b1 <> b2)
+
+instance Monoid CountResult where
+  mempty = CountResult mempty mempty mempty
+
+instance Show CountResult where
+  show (CountResult (Sum l) (WordsResult _ w _) (Sum b)) =
+    show l <> "\t" <> show w <> "\t" <> show b
 
 run :: String -> IO ()
 run fp = IO.withBinaryFile fp IO.ReadMode $ \h -> do
@@ -28,14 +52,15 @@ run fp = IO.withBinaryFile fp IO.ReadMode $ \h -> do
   print $ count str
 
 count :: BSL.ByteString -> CountResult
-count s = C8.foldl' go (CountResult 0 0 False 0) s
+count s = C8.foldl' go mempty s
   where
     go :: CountResult -> Char -> CountResult
-    go (CountResult ls wc prevW bs) c =
+    go prevR c =
       let
         addL | c == '\n' = 1
              | otherwise = 0
-        sp = isSpace c
-        addW | not sp && prevW = 1
+        nsp = not $ isSpace c
+        addW | nsp = 1
              | otherwise = 0
-      in CountResult (ls + addL) (wc + addW) (not sp) (bs + 1)
+        newR = CountResult (Sum addL) (WordsResult nsp addW nsp) (Sum 1)
+      in prevR <> newR
